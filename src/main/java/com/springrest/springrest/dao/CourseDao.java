dc48b80f0b422f36aa24d842507388809829f804
Deep Awasthi <da.madskull@gmail.com>
2026-07-03 17:52:21 +0530

    Remove files

--- src/main/java/com/springrest/springrest/dao/CourseDao.java
+++
@@ -1,8 +0,0 @@
-package com.springrest.springrest.dao;
-
-import com.springrest.springrest.entities.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseDao extends JpaRepository<Course, Long> {
-
-}
